b919da89d9886b1201de4588d0fb4d820769cfaf
hamdi <hamdigatri92@gmail.com>
2019-09-02 23:01:18 +0100

    Create page head section.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 import React from "react";
+import Head from "next/head";
 
-const Home = () => <div></div>;
+const Home = () => (
+  <div>
+    <Head>
+      <title>League Table</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/purecss@0.6.1/build/pure-min.css"
+      />
+    </Head>
+  </div>
+);
 
 export default Home;
