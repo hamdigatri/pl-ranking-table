13d58fedb49cb489c75dfdca12b6c19cec26c2bd
hamdi <hamdigatri92@gmail.com>
2019-09-03 23:15:44 +0100

    create getInitialProps function.

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,31 @@
 import React from "react";
 import Head from "next/head";
+import axios from "axios";
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>League Table</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/purecss@0.6.1/build/pure-min.css"
-      />
-    </Head>
-  </div>
-);
+class Home extends React.Component {
+  static async getInitialProps() {
+    const res = await axios.get(
+      "http://api.football-data.org/v2/competitions/2003/matches?matchday=1"
+    );
+    return { data: res.data };
+  }
 
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>League Table</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/purecss@0.6.1/build/pure-min.css"
+          />
+        </Head>
+      </div>
+    );
+  }
+}
 export default Home;
