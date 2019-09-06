6bb372bb4f8dee36b5e74f68ed09a2e271a441fa
hamdi <hamdigatri92@gmail.com>
2019-09-07 00:00:02 +0100

    Display selected team details.

--- pages/details.js
+++ pages/details.js
@@ -7,6 +7,17 @@ const Details = props => {
   return (
     <div>
       <h1>Team detail</h1>
+      <img src={team.team.crestUrl} style={{ width: 100, height: 100 }} />
+      <ul>
+        <li>Name : {team.team.name}</li>
+        <li>Played Games : {team.playedGames}</li>
+        <li>Won : {team.won}</li>
+        <li>Draw : {team.draw}</li>
+        <li>Lost : {team.lost}</li>
+        <li>Goals for : {team.goalsFor}</li>
+        <li>Goals against : {team.goalsAgainst}</li>
+        <li>Goal diffrence : {team.goalDifference}</li>
+      </ul>
     </div>
   );
 };
