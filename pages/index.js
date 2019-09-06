ca3e717e337b392c7f9c0e01dc5b2323adf27d23
hamdi <hamdigatri92@gmail.com>
2019-09-06 23:13:20 +0100

    render and formating

--- pages/index.js
+++ pages/index.js
@@ -6,37 +6,22 @@ import AppContext from "../components/appContext";
 const Home = () => {
   const { competition, season, standings } = useContext(AppContext);
   return (
-    console.log(props.data),
-    (
-      <div>
-        <Head>
-          <title>League Table</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/purecss@0.6.1/build/pure-min.css"
-          />
-        </Head>
-        <h1>{props.data.competition.name}</h1>
-        <h2>
-          Season: {parseInt(props.data.season.startDate)} -{" "}
-          {parseInt(props.data.season.endDate)}
-        </h2>
-        <StandingsTable total={props.data.standings[0]} />
-      </div>
-    )
+    <div>
+      <Head>
+        <title>League Table</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/purecss@0.6.1/build/pure-min.css"
+        />
+      </Head>
+      <h1>{competition}</h1>
+      <h2>
+        Season: {parseInt(season.startDate)} - {parseInt(season.endDate)}
+      </h2>
+      <StandingsTable total={standings[0]} />
+    </div>
   );
-};
-
-Home.getInitialProps = async () => {
-  const res = await axios.get(
-    "http://api.football-data.org/v2/competitions/2021/standings",
-    { headers: { "X-Auth-Token": "57c8cb7275dd4ab695bc3f3d9b23ba92" } }
-  );
-  return { data: res.data };
 };
 
 export default Home;
