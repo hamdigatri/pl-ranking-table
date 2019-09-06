ff62912027cd2886c013dc9cf46df50cf48e57b6
hamdi <hamdigatri92@gmail.com>
2019-09-06 23:43:18 +0100

    Get team data based on selected id.

--- pages/details.js
+++ pages/details.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import AppContext from "../components/appContext";
 
 const Details = props => {
+  const { standings } = useContext(AppContext);
+  const team = standings[standings.findIndex(item => item.team.id == props.id)];
   return (
     <div>
       <h1>Team detail</h1>
