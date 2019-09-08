5ab440231f0bd1f2270f08e9f5b986782e4a4931
hamdi <hamdigatri92@gmail.com>
2019-09-08 22:20:26 +0100

    Details page styling.

--- pages/details.js
+++ pages/details.js
@@ -7,19 +7,22 @@ const Details = props => {
   const team = standings[standings.findIndex(item => item.team.id == props.id)];
   return (
     <Layout>
-      <div>
-        <h1>Team detail</h1>
-        <img src={team.team.crestUrl} style={{ width: 100, height: 100 }} />
-        <ul>
-          <li>Name : {team.team.name}</li>
-          <li>Played Games : {team.playedGames}</li>
-          <li>Won : {team.won}</li>
-          <li>Draw : {team.draw}</li>
-          <li>Lost : {team.lost}</li>
-          <li>Goals for : {team.goalsFor}</li>
-          <li>Goals against : {team.goalsAgainst}</li>
-          <li>Goal diffrence : {team.goalDifference}</li>
-        </ul>
+      <div className="details-container">
+        <div>
+          <img src={team.team.crestUrl} style={{ width: 250, height: 250 }} />
+        </div>
+        <div>
+          <ul>
+            <li>Name : {team.team.name}</li>
+            <li>Played Games : {team.playedGames}</li>
+            <li>Won : {team.won}</li>
+            <li>Draw : {team.draw}</li>
+            <li>Lost : {team.lost}</li>
+            <li>Goals for : {team.goalsFor}</li>
+            <li>Goals against : {team.goalsAgainst}</li>
+            <li>Goal diffrence : {team.goalDifference}</li>
+          </ul>
+        </div>
       </div>
     </Layout>
   );
