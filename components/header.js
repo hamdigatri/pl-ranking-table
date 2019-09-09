7fff2e97b5784c3490cce407531795f858ac77b8
hamdi <hamdigatri92@gmail.com>
2019-09-09 23:42:40 +0100

    Enhance page header.

--- components/header.js
+++ components/header.js
@@ -1,26 +1,29 @@
 import React, { useContext } from "react";
 import AppContext from "./appContext";
 import Link from "next/link";
+import Router from "next/router";
 const Header = props => {
   const { season, competition } = useContext(AppContext);
   return (
     <header>
       {props.goBack && (
         <div style={{ display: "flex", flex: 1, justifyContent: "center" }}>
-          <Link href="/">
+          <button onClick={() => Router.back()}>
             <img
               src="../static/left-arrow.svg"
               alt="go back"
               style={{ width: 25, height: 25, cursor: "pointer" }}
             />
-          </Link>
+          </button>
         </div>
       )}
-      <img
-        src="../static/logo.svg"
-        alt="premier league logo"
-        className="logo"
-      />
+      <Link href="/">
+        <img
+          src="../static/logo.svg"
+          alt="premier league logo"
+          className="logo"
+        />
+      </Link>
       <div className="title-container" style={props.goBack ? { flex: 1 } : {}}>
         <h1>{competition}</h1>
         <h2>
